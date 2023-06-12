2275fe5712156a64b31257afa0ae227cdb7c1f34
mamta823 <mamtadevi.webethics@gmail.com>
2023-06-12 13:54:49 +0530

    fix route error

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import {
   Route
 } from "react-router-dom";
 import Home from './pages/home';
-import Detail from './pages/detail';
 function App() {
 
   return (
@@ -15,7 +14,6 @@ function App() {
       {/* <h1>hello world</h1> */}
       <Routes>
         <Route path="/home" element={<Home />} />
-        <Route path="/detail/:id" element={<Detail />} />
       </Routes>
     </div>
   );
